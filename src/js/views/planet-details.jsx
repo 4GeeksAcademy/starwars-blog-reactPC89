ca0c86fa12a5d16a39cc8b68b42ef8b86162853a
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-09-14 05:55:49 +0000

    vista detalles

--- src/js/views/planet-details.jsx
+++ src/js/views/planet-details.jsx
@@ -1,35 +1,48 @@
-import React, {useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { useParams } from "react-router";
 
 export const PlanetDetails = () => {
 
-    const {id}= useParams()
-    const {actions, store} = useContext (Context)
+    const { id } = useParams();
+    const { actions, store } = useContext(Context);
 
-    useEffect(()=>{
-     actions.obtenerPlanetaDetails(id)
-    }, [])
+    useEffect(() => {
+        actions.obtenerPlanetaDetails(id);
+    }, []);
 
-    console.log(store.planet.name);
     return (
         <div className="text-center m-2">
             <div className="row">
-                <div className="col-3">
-                    <div className="card" style={{ width: "18rem", flex:"none", margin:"10px" }}>
-                        <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} className="card-img-top" alt={name} />
-                        <div className="card-body">
-                            <h5 className="card-title">Name: {store.planet.name}</h5>
-                            <p className="card-text">Climate: {store.planet.climate}</p>
-                            <p className="card-text">Population: {store.planet.population}</p>
-                            <p className="card-text">Orbital Period: {store.planet.orbital_period}</p>
-                            <p className="card-text">Rotation Period: {store.planet.rotation_period}</p>
-                            <p className="card-text">Diameter: {store.planet.diameter}</p>                                                       
-                        </div>
+                <div className="col-md-4">
+                    <div className="card" style={{ width: "100%", margin: "10px" }}>
+                        <img
+                            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                            className="card-img-top"
+                            alt={store.planet.name}
+                        />
+                    </div>
+                </div>
+
+                <div className="col-md-8 d-flex flex-column p-4 justify-content-center">
+                    <h3 className="card-title text-start text-light">{store.planet.name}</h3>
+                    <p className="text-start text-light">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat? Quas quia, minima omnis minus odio labore harum incidunt nam, dolore tempore reiciendis animi autem. Necessitatibus culpa ex odit ut?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In natus ratione laborum explicabo aliquam suscipit, consequatur voluptatibus iusto aut ipsam! Pariatur excepturi animi sit illum quod perferendis beatae ducimus quo?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat? Quas quia, minima omnis minus odio labore harum incidunt nam, dolore tempore reiciendis animi autem. Necessitatibus culpa ex odit ut?
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula quam nec odio suscipit, sit amet gravida mi dignissim.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula quam nec odio suscipit, sit amet gravida mi dignissim.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In natus ratione laborum explicabo aliquam suscipit, consequatur voluptatibus iusto aut ipsam! Pariatur excepturi animi sit illum quod perferendis beatae ducimus quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat? Quas quia, minima omnis minus odio labore harum incidunt nam, dolore tempore reiciendis animi autem. Necessitatibus culpa ex odit ut?
+                    </p>
+                    <div className="d-flex text-warning">
+                        <p className="card-text me-4">Climate: {store.planet.climate}</p>
+                        <p className="card-text me-4">Population: {store.planet.population}</p>
+                        <p className="card-text me-4">Orbital Period: {store.planet.orbital_period}</p>
+                        <p className="card-text me-4">Rotation Period: {store.planet.rotation_period}</p>
+                        <p className="card-text me-4">Diameter: {store.planet.diameter}</p>
                     </div>
                 </div>
             </div>
         </div>
-
-    )
+    );
 };
